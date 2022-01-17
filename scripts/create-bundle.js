bb44dafe13edaf21d0c55d97f7f8565b1358c789
Patrick Organ <patrick@permafrost.dev>
2022-01-16 20:41:18 -0500

    code cleanup and minor tweaks

--- scripts/create-bundle.js
+++ scripts/create-bundle.js
@@ -1,27 +1,29 @@
 const fs = require('fs');
 const archiver = require('archiver');
 
+process.stdout.write(`Creating bundle...\n`);
+
 const output = fs.createWriteStream(__dirname + '/../project.zip');
 
 const archive = archiver('zip', {
-    zlib: { level: 9 } // Sets the compression level.
+    zlib: { level: 9 }, // Sets the compression level.
 });
 
-output.on('close', function() {
+output.on('close', function () {
     const size = (archive.pointer() / 1024).toFixed(2) + ' kb';
 
-    console.log(`created project.zip for submission (${size}).`);
+    process.stdout.write(`Created project.zip for submission (${size}).\n`);
 });
 
-archive.on('warning', function(err) {
-  if (err.code === 'ENOENT') {
-    // log warning
-  } else {
-    throw err;
-  }
+archive.on('warning', function (err) {
+    if (err.code === 'ENOENT') {
+        // log warning
+    } else {
+        throw err;
+    }
 });
 
-archive.on('error', function(err) {
+archive.on('error', function (err) {
     throw err;
 });
 
@@ -29,7 +31,5 @@ archive.pipe(output);
 
 archive.file(__dirname + '/../src/Script.js', { name: 'Script.js' });
 archive.file(__dirname + '/../src/ScriptTraits.js', { name: 'ScriptTraits.js' });
-
-//archive.directory(__dirname + '/../src/', 'src');
 
 archive.finalize();
