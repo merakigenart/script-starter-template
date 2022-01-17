2f7c8967cb268bde98d8f0c818618bb03f2b5353
Patrick Organ <patinthehat@users.noreply.github.com>
2022-01-16 20:15:46 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// This file is important for the automatic rendering of your scripts, so it shouldn't be modified.
+
 import { Script } from './Script';
 import { ScriptTraits } from './ScriptTraits';
 
